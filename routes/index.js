45b6adce4ff4dc447c493672dd0cb8d34350ccbf
camposmm <mcamposdossantos@byupathway.edu>
2025-08-09 20:34:19 -0300

    week06

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('Welcome to the page'));
 
+// ✅ add this line
+router.get('/healthz', (req, res) => res.status(200).send('ok'));
+
 router.use('/student', require('./student'));
 router.use('/instructor', require('./instructor'));
 router.use('/course', require('./course'));
 router.use('/enrollments', require('./enrollment'));
 
-// ✅ Swagger UI
 router.use('/api-docs', require('./swagger'));
-
 module.exports = router;
